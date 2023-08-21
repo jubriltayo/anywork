f144056726f18f4496c958f641f1991e5cc9684f
Tayo Jubril <jubriltayo@gmail.com>
2023-08-21 16:17:23 +0100

    trial

--- src/anywork/anywork/urls.py
+++ src/anywork/anywork/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from pages.views import home_view
 from professionals.views import professional_create_view
 
@@ -26,3 +27,5 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
