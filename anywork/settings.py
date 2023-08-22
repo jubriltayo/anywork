0660db72c478de214241f40f3701cc7475d94e09
Tayo Jubril <jubriltayo@gmail.com>
2023-08-22 22:48:28 +0100

    superuser

--- anywork/settings.py
+++ anywork/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'images.apps.ImageConfig',
+    'images.apps.ImagesConfig',
     
     # third party app
     'bootstrap5',
