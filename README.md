c51671384ffd46ab17888d5383999548204d4d5b
Tayo Jubril <jubriltayo@gmail.com>
2023-09-11 14:06:54 +0100

    clean up

---
+++ README.md
@@ -0,0 +1,28 @@
+# AnyWork
+
+## Introduction
+This is a web app where professionals can register andoffer their services to clients.
+
+[anywork](anywork.onrender.com)
+
+[blog post](https://jubriltayo.medium.com/a-review-my-first-django-app-anywork-6deb96ec0371) 
+
+[linkedIn](https://linkedin.com/in/jubril-tayo)
+
+
+## Core Technology
+### Installation
+It was built using:
+* #### django - backend
+        pip install django
+* #### bootstrap - frontend
+        pip install django-bootstrap-v5
+* #### postgreSQL - database
+
+### Usage
+The project was carried out in a virtual environment
+
+        virtualenv venv
+
+## Preview
+![Home Page](anywork1.png)
--- professionals/forms.py
+++ professionals/forms.py
@@ -2,53 +2,8 @@ from django import forms
 from .models import Professional
 
 class ProfessionalForm(forms.ModelForm):
-    # OPTIONS = [
-    #     ('Tutorship', 'Tutorship'),
-    #     ('Transportation', 'Transportation'),
-    #     ('Food', 'Food'),
-    #     ('Cleaning', 'Cleaning')
-    # ]
-    # name                = forms.CharField(label='', max_length=128, 
-    #                                       widget=forms.TextInput(
-    #                                           attrs={
-    #                                               "placeholder": "Name"
-    #                                           }
-    #                                       ))
-    # email               = forms.EmailField(label='', 
-    #                                        widget=forms.EmailInput(
-    #                                            attrs={
-    #                                                "placeholder": "Email"
-    #                                            }
-    #                                        ))
-    # phone_number        = forms.CharField(label='', max_length=20,
-    #                                       widget=forms.TextInput(
-    #                                           attrs={
-    #                                               "placeholder": "Phone number"
-    #                                           }
-    #                                         ))
-    # address             = forms.CharField(label='',
-    #                                       widget=forms.Textarea(
-    #                                           attrs={
-    #                                               "placeholder": "Address", 
-    #                                               "rows": 10,
-    #                                               "cols": 60
-    #                                           }
-    #                                       ))
-    # service             = forms.ChoiceField(widget=forms.RadioSelect, choices=OPTIONS)
-    # years_of_experience = forms.IntegerField(min_value=0, max_value=50)
-    # qualification       = forms.CharField(label='', 
-    #                                       widget=forms.Textarea(
-    #                                           attrs={
-    #                                               "placeholder": "Qualification", 
-    #                                               "rows": 10,
-    #                                               "cols": 40
-    #                                           }
-    #                                       ))
-    # price               = forms.IntegerField(initial=500, 
-    #                                          min_value=0, 
-    #                                          max_value=10000000
-    #                                          )
-    
+    pass
+
     class Meta:
         model = Professional
         fields = [
